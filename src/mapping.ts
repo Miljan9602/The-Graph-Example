ddc3eddea7daf4df536a9e3df77497eee2ecc029
Miljan9602 <rakitamiljan@yahoo.com>
2021-05-22 14:46:38 +0200

    Removed non used code.

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,29 +2,6 @@ import {SetValue} from '../generated/Storage/Storage'
 import { Storage } from '../generated/schema'
 import { log } from "@graphprotocol/graph-ts"
 
-/*
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
-
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
-*/
-
-
 export function handleSetValue(event: SetValue): void {
 
     let id = event.params.value.toHex()
